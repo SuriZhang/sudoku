51c4ec15b9b6da8ac47b6225c75c704bc050e716
SuriZhang <surizhang1224@gmail.com>
2023-10-26 13:21:18 +0800

    improve UI on mode buttons

--- app/pages/game.tsx
+++ app/pages/game.tsx
@@ -18,17 +18,23 @@ export default function Game() {
 			</div>
 			<div className="mb-8 mt-2 flex justify-center">
 				<button
-					className="mr-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded"
+					className={`mr-2 
+					${currentMode === "INSERT" ? "bg-blue-500" : "bg-blue-300"} 
+					hover:bg-blue-700 text-white font-bold py-2 px-6 rounded`}
 					onClick={() => handleModeChange("INSERT")}>
 					Insert Mode (i)
 				</button>
 				<button
-					className="mr-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded"
+					className={`mr-2 
+					${currentMode === "MARK" ? "bg-blue-500" : "bg-blue-300"} 
+					hover:bg-blue-700 text-white font-bold py-2 px-6 rounded`}
 					onClick={() => handleModeChange("MARK")}>
 					Mark Mode (m)
 				</button>
 				<button
-					className="mr-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded"
+					className={`mr-2 
+					${currentMode === "SELECT" ? "bg-blue-500" : "bg-blue-300"} 
+					hover:bg-blue-700 text-white font-bold py-2 px-6 rounded`}
 					onClick={() => handleModeChange("SELECT")}>
 					Select Mode (s)
 				</button>
