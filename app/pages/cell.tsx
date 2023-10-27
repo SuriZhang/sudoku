bfeddae548af57ef073582b677d45e1757b39ee8
SuriZhang <surizhang1224@gmail.com>
2023-10-27 19:53:19 +0800

    mark is displayed correctly, now accepting input

--- app/pages/cell.tsx
+++ app/pages/cell.tsx
@@ -1,6 +1,11 @@
 "use client";
 
-import React, { useContext, useEffect, useState } from "react";
+import React, {
+	KeyboardEventHandler,
+	useContext,
+	useEffect,
+	useState,
+} from "react";
 import { Mark } from "./mark";
 import { GridInfo } from "./useCalculateGridInfo";
 import { ModeContext } from "./modeContext";
@@ -14,18 +19,24 @@ interface CellProps extends GridInfo {
 // information that is exclusive to current cell
 export const Cell = (props: CellProps) => {
 	// marks for cell
-    const [markValues, setMarkValues] = useState<number[]>([]);
-    
-    const currentMode = useContext(ModeContext)
+	const [markValues, setMarkValues] = useState<number[]>([]);
 
-	const onMarkValueEnter = (value: number) => {
-		// if the mark already exists, remove it
-		if (markValues.includes(value)) {
-			setMarkValues(markValues.filter((mark: number) => mark !== value));
-			return;
-		} else {
-			// add mark to the cell
-			setMarkValues([...markValues, value]);
+	const currentMode = useContext(ModeContext);
+
+	const handleOnMarkKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		console.log(`e.key = ${e.key}`);
+		if (e.key.match(/[1-9]/)) {
+			let inputValue: number = parseInt(e.key.slice(0, 1));
+			if (markValues.includes(inputValue)) {
+				console.log(`xxx, value = ${inputValue}`);
+				setMarkValues(
+					markValues.filter((mark: number) => mark !== inputValue)
+				);
+			} else {
+				console.log(`yyy, value = ${inputValue}`);
+				// add mark to the cell
+				setMarkValues([...markValues, inputValue]);
+			}
 		}
 		console.log(`markValues = ${markValues}`);
 	};
@@ -38,19 +49,8 @@ export const Cell = (props: CellProps) => {
 			console.log(
 				`inputValue = ${inputValue}, cellValue = ${props.value}`
 			);
-            console.log(`currentMode = ${currentMode}`)
-			switch (currentMode) {
-				case "INSERT":
-					props.onValueChange(props.x, props.y, inputValue);
-					break;
-				case "MARK":
-					onMarkValueEnter(inputValue);
-					break;
-				case "SELECT":
-					break;
-				default:
-					break;
-			}
+			console.log(`currentMode = ${currentMode}`);
+			props.onValueChange(props.x, props.y, inputValue);
 		}
 	};
 
@@ -58,40 +58,31 @@ export const Cell = (props: CellProps) => {
 		props.onClick(props.x, props.y);
 	};
 
-	const initMarkValues = () => {
-		const markValues: number[] = [];
-		for (let i = 1; i <= 9; i++) {
-			markValues.push(i);
-		}
-		setMarkValues(markValues);
-	};
-
 	// rendering cell
-	useEffect(() => initMarkValues(), []);
-
 	if (currentMode === "MARK" && props.value === 0) {
 		return (
 			<>
 				<div
+					tabIndex={0}
 					className={`flex h-full w-full justify-center aspect-square 
-                border boarder-1 border-black p-0 text-sm text-center grid grid-cols-3 grid-rows-3 gap-0
-                row-start-${props.x + 1} col-start-${props.y + 1}
-                ${
-					(props.x + 1) % 3 === 0 &&
-					props.x !== 8 &&
-					"border-b-2 border-black"
-				}
-                ${
-					(props.y + 1) % 3 === 0 &&
-					props.y !== 8 &&
-					"border-r-2 border-black"
-				}
-                ${props.isConflict ? "text-red-500" : "text-black"}
-                ${props.isSelected && "bg-yellow-200"}
-            `}
-					onChange={handleOnValueChange}
+                    border boarder-1 border-black p-0 text-sm text-center grid grid-cols-3 grid-rows-3 gap-0
+                    row-start-${props.x + 1} col-start-${props.y + 1}
+                    ${
+						(props.x + 1) % 3 === 0 &&
+						props.x !== 8 &&
+						"border-b-2 border-black"
+					}
+                    ${
+						(props.y + 1) % 3 === 0 &&
+						props.y !== 8 &&
+						"border-r-2 border-black"
+					}
+                    ${props.isConflict ? "text-red-500" : "text-black"}
+                    ${props.isSelected && "bg-yellow-200"}
+                    `}
+					onKeyDown={handleOnMarkKeyDown}
 					onClick={handleOnClick}>
-					{markValues.map((markValue: number) => (
+					{...markValues.map((markValue: number) => (
 						<Mark
 							key={`${props.x}*9+${props.y}+${markValue}}`}
 							{...props}
@@ -106,21 +97,21 @@ export const Cell = (props: CellProps) => {
 			<>
 				<input
 					className={`flex h-full w-full justify-center font-bold aspect-square 
-                border boarder-1 border-black p-0 text-2xl text-center
-                row-start-${props.x + 1} col-start-${props.y + 1}
-                ${
-					(props.x + 1) % 3 === 0 &&
-					props.x !== 8 &&
-					"border-b-2 border-black"
-				}
-                ${
-					(props.y + 1) % 3 === 0 &&
-					props.y !== 8 &&
-					"border-r-2 border-black"
-				}
-                ${props.isConflict ? "text-red-500" : "text-black"}
-                ${props.isSelected && "bg-yellow-200"}
-            `}
+                        border boarder-1 border-black p-0 text-2xl text-center
+                        row-start-${props.x + 1} col-start-${props.y + 1}
+                        ${
+							(props.x + 1) % 3 === 0 &&
+							props.x !== 8 &&
+							"border-b-2 border-black"
+						}
+                        ${
+							(props.y + 1) % 3 === 0 &&
+							props.y !== 8 &&
+							"border-r-2 border-black"
+						}
+                        ${props.isConflict ? "text-red-500" : "text-black"}
+                        ${props.isSelected && "bg-yellow-200"}
+                    `}
 					value={props.value === 0 ? "" : props.value}
 					onChange={handleOnValueChange}
 					readOnly={!props.isEditable}
