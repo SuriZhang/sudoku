2ff8de4c8cac8b9e71ee914556fe568118d16929
SuriZhang <surizhang1224@gmail.com>
2023-10-29 19:36:47 +0800

    retrieved puzzles from supabase

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,34 @@
 
 import React, { useState, useEffect } from "react";
 
-import Image from "next/image";
 import Game from "./pages/game";
+import supabase from "./utils/supabase";
 
-export default function Home() {
-    const [boardSize, setBoardSize] = useState<number>(9);
+async function getPuzzles() {
+	let { data: puzzles, error } = await supabase
+		.from("sudoku_puzzles")
+		.select("puzzle");
+	
+	if (error) {
+		throw new Error(error.message);
+	}
+	return puzzles?.map((puzzle) => puzzle.puzzle) ?? [];
+}
 
-    // Function to update the boardSize
-    const updateBoardSize = (newSize: number) => {
-        setBoardSize(newSize);
-    };
 
-    return (
+export default async function Home() {
+	
+	const puzzles = await getPuzzles();
+	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<h1 className="font-bold">Sudoku Game</h1>
 
 			<div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
 				<Game />
+				<div>
+				{puzzles?.map((puzzle, index) => (puzzle && <p key={index}>{puzzle}</p>))}
+
+				</div>
 				<button className="flex bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded">
 					show answer
 				</button>
