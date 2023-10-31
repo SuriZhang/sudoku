4054c433ef4a90cbc81e99e2e8d1f03a51b930cd
SuriZhang <surizhang1224@gmail.com>
2023-10-31 15:27:36 +0800

    change text-color and bg-color

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,10 @@ import Game from "./pages/game";
 
 export default async function Home() {
 	return (
-		<main className="flex flex-col items-center justify-between p-24 h-screen">
-			<h1 className="font-bold text-xl font-mono">Sudoku Game</h1>
+		<main className="flex flex-col items-center justify-between p-24 h-screen bg-blue-100">
+			<h1 className="font-bold text-xl font-mono text-blue-500">
+				Sudoku Game
+			</h1>
 			<div className="z-10 w-2/3 max-h-screen items-center justify-between font-mono text-sm lg:flex lg:w-1/2">
 				<Game />
 			</div>
