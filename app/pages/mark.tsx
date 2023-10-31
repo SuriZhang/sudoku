8814bd06413b54da245c3921539f43d08a575068
Anxing (Suri) Zhang <surizhang1224@gmail.com>
2023-10-31 20:30:58 +0800

    change mark text-color

--- app/pages/mark.tsx
+++ app/pages/mark.tsx
@@ -9,7 +9,7 @@ export const Mark = (props: MarkProps) => {
 	return (
 		<div
 			className={`row-start-${Math.floor((props.markValue - 1) / 3) + 1}
-			 col-start-${((props.markValue - 1) % 3) + 1} text-sm`}>
+			 col-start-${((props.markValue - 1) % 3) + 1} text-sm text-black`}>
 			{props.markValue === 0 ? "" : props.markValue}
 		</div>
 	);
